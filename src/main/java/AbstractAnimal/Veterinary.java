07cff6db21b091212b1a3a289325b0527f22afa9
Ian Pillado <22670007@iguala.tecnm.mx>
2023-04-21 09:36:01 -0600

    "Se creo la clase Veterinary"

--- src/main/java/AbstractAnimal/Veterinary.java
+++ src/main/java/AbstractAnimal/Veterinary.java
@@ -7,7 +7,7 @@ public class Veterinary {
         Rooster claudio=new Rooster("Claudio","Blanco con rojo",1);
 
         System.out.println("\n--Ejemplo de polimorfismo usando clase abstracta--");
-        System.out.println("Invocando al metodo toString:\n");
+        System.out.println("Invocando al metodo toString:");
         System.out.println(garfield.toString());
         System.out.println(felix.toString());
         System.out.println(firulais.toString());
