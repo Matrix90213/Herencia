5a3fcf410b7f28abb946505344082489751ee8d2
Ian Pillado <22670007@iguala.tecnm.mx>
2023-05-12 08:08:11 -0600

    "Package Array"

---
+++ src/main/java/Array/Dado.java
@@ -0,0 +1,25 @@
+package Array;
+import java.security.SecureRandom;
+public class Dado {
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+        int[] tiro = new int[7]; //
+        final int LANZAMIENTOS = 10000;
+        int lanzamiento;
+        tiro[1] = 0;
+        tiro[2] = 0;
+        tiro[3] = 0;
+        tiro[4] = 0;
+        tiro[5] = 0;
+        tiro[6] = 0;
+
+        for (int i = 0; i < LANZAMIENTOS; i++) {
+            lanzamiento = random.nextInt(6) + 1;
+            tiro[lanzamiento] = tiro[lanzamiento] + 1;
+        }
+        for (int i = 1; i <tiro.length;i++) {
+            System.out.println("La cara "+i+" del Dado cayo "+tiro[i]+" veces");
+        }
+    }
+}
+
